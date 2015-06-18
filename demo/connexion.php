0644a20e2f007efd0c67aa6880321ee6f0ba3525
AlexandrePain <Alexandre.pain78@gmail.com>
2015-06-18 11:03:28 +0200

    demo
    comprend : connexion, déco, inscription, profile.

---
+++ demo/connexion.php
@@ -0,0 +1,71 @@
+﻿<?php
+session_start();
+$titre="Connexion";
+$lvl=(isset($_SESSION['level']))?(int) $_SESSION['level']:1;
+$id=(isset($_SESSION['id']))?(int) $_SESSION['id']:0;
+$pseudo=(isset($_SESSION['pseudo']))?$_SESSION['pseudo']:'';
+include("includes/connexBDD.php");
+include("includes/functions.php");
+
+echo '<p><i>Vous êtes ici</i> : <a href="./index.php">Index du forum</a> --> Connexion';
+define('ERR_IS_CO','Vous êtes déja connecté');
+echo '<h1>Connexion</h1>';
+if ($id!=0) erreur(ERR_IS_CO);
+if (!isset($_POST['pseudo'])) //On est dans la page de formulaire
+{
+	echo '<form method="post" action="connexion.php">
+	<fieldset>
+	<legend>Connexion</legend>
+	<p>
+	<label for="pseudo">Pseudo :</label><input name="pseudo" type="text" id="pseudo" /><br />
+	<label for="password">Mot de Passe :</label><input type="password" name="password" id="password" />
+	</p>
+	</fieldset>
+	<p><input type="submit" value="Connexion" /></p></form>
+	<a href="./register.php">Pas encore inscrit ?</a>
+	 
+	</div>
+	</body>
+	</html>';
+}
+else
+{
+    $message='';
+    if (empty($_POST['pseudo']) || empty($_POST['password']) ) //Oublie d'un champ
+    {
+        $message = '<p>une erreur s\'est produite pendant votre identification.
+	Vous devez remplir tous les champs</p>
+	<p>Cliquez <a href="./connexion.php">ici</a> pour revenir</p>';
+    }
+    else //On check le mot de passe
+    {
+        $query=$db->prepare('SELECT password, id_uti, pseudo, niveau
+        FROM utilisateur WHERE pseudo = :pseudo');
+        $query->bindValue(':pseudo',$_POST['pseudo'], PDO::PARAM_STR);
+        $query->execute();
+        $data=$query->fetch();
+	if ($data['password'] == sha1($_POST['password'])) // Acces OK !
+	{
+	    $_SESSION['pseudo'] = $data['pseudo'];
+	    $_SESSION['level'] = $data['niveau'];
+	    $_SESSION['id'] = $data['id_uti'];
+	    $message = '<p>Bienvenue '.$data['pseudo'].', 
+			vous êtes maintenant connecté!</p>
+			<p>Cliquez <a href="./index.php">ici</a> 
+			pour revenir à la page d accueil</p>';  
+	}
+	else // Acces pas OK !
+	{
+	    $message = '<p>Une erreur s\'est produite 
+	    pendant votre identification.<br /> Le mot de passe ou le pseudo 
+            entré n\'est pas correcte.</p><p>Cliquez <a href="./connexion.php">ici</a> 
+	    pour revenir à la page précédente
+	    <br /><br />Cliquez <a href="./index.php">ici</a> 
+	    pour revenir à la page d accueil</p>';
+	}
+    $query->CloseCursor();
+    }
+    echo $message.'</div></body></html>';
+
+}
+?>
